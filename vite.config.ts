b2a30b3d0536503e7e623a04fa621b9797c82ef2
Saurabh Kumar Jha <89315019+sjregistered@users.noreply.github.com>
2026-02-07 03:49:36 +0530

    first commit - second

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,21 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
 
-const config = defineConfig({
+export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   plugins: [
-    devtools(),
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-
-    tanstackStart(),
     viteReact(),
   ],
+  build: {
+    outDir: 'dist',
+  },
 })
-
-export default config
